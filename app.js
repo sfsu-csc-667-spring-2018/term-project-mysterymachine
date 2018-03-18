bffa3e52a3e8de0a488ad41f7e7dd3b0757c3605
phuonglinh83 <phuonglinh83@gmail.com>
2018-03-17 22:59:59 -0700

    Add app.js and basic server, set up sequlize

---
+++ app.js
@@ -0,0 +1,23 @@
+if(process.env.NODE_ENV === 'development') {
+  require("dotenv").config();
+}
+
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+// Set up the express app
+const app = express();
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setup a default catch-all route that sends back a welcome message.
+app.get('*', (req, res) => res.status(200).send(
+  '<h1>Welcome to CSC667 MisteryMachine team</h1>'));
+
+module.exports = app;
