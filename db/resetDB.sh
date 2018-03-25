be98a5a790f473f8851327176e913680357e5106
phuonglinh83 <phuonglinh83@gmail.com>
2018-03-25 13:38:59 -0700

    Clean myapp, code was moved to root directory

--- db/resetDB.sh
+++
@@ -1,6 +0,0 @@
-#cd to the script location first
-cd $(dirname "$0")
-dropdb 667termproject
-createdb 667termproject
-psql -d 667termproject -a -f schema/db_setup.sql
-psql -d 667termproject -a -f schema/card_data_inserts.sql
