00c2a0e7b66abdb28be81e5213a6c8cb4a563e06
unknown <isak.torgersen@gmail.com>
2018-04-11 18:51:35 -0700

    Big changes to game_table. Player hand now loads based on js and a js object. Changed icon for opponenet deck and changed font size of username. Changed the chat. Chat prototype working.

---
+++ js/game_table.js
@@ -0,0 +1,51 @@
+
+$(function() {
+  /*  $( ".opponent_area" ).hover(
+  function() {
+    $( this ).children("img").width("35%");
+    $( this ).children("p").css({"font-size": "2em"});
+    $( this ).children("p").css({"text-shadow": "0px 0px 8px rgba(82, 168, 236, 0.6)"});
+    $( this ).children("p").css({"border-color": "rgba(82, 168, 236, 0.8)"});  
+  }    
+);
+  $( ".opponent_area" ).mouseout(
+  function() {
+    $( this ).children("img").width("25%");
+    $( this ).children("p").css({"font-size": "1em"});
+    $( this ).children("p").css({"text-shadow": "none"});
+    $( this ).children("p").css({"border-color": "none"});  
+  }    
+);
+    $( ".opponent_area p").hover(
+  function() {
+    $( this ).parent().children("img").width("35%");
+    $( this ).parent().children("p").css({"font-size": "2em"});
+    $( this ).parent().children("p").css({"text-shadow": "0px 0px 8px rgba(82, 168, 236, 0.6)"});
+    $( this ).parent().children("p").css({"border-color": "rgba(82, 168, 236, 0.8)"});  
+  }    
+);
+  $( ".opponent_area p" ).mouseout(
+  function() {
+    $( this ).parent().children("img").width("25%");
+    $( this ).parent().children("p").css({"font-size": "1em"});
+    $( this ).parent().children("p").css({"text-shadow": "none"});
+    $( this ).parent().children("p").css({"border-color": "none"});  
+  }    
+);
+     $( ".opponent_area img" ).hover(
+  function() {
+    $( this ).parent().children("img").width("35%");
+    $( this ).parent().children("p").css({"font-size": "2em"});
+    $( this ).parent().children("p").css({"text-shadow": "0px 0px 8px rgba(82, 168, 236, 0.6)"});
+    $( this ).parent().children("p").css({"border-color": "rgba(82, 168, 236, 0.8)"});  
+  }    
+);
+  $( ".opponent_area img" ).mouseout(
+  function() {
+    $( this ).parent().children("img").width("25%");
+    $( this ).parent().children("p").css({"font-size": "1em"});
+    $( this ).parent().children("p").css({"text-shadow": "none"});
+    $( this ).parent().children("p").css({"border-color": "none"});  
+  }    
+);*/
+});
