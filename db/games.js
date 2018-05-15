c06359dd1ea5d5a10b90942d1a238ac0ccc89b93
unknown <isak.torgersen@gmail.com>
2018-05-15 16:19:58 -0700

    Completed most of the SQL statments outlined in the db/game.js.

--- db/games.js
+++ db/games.js
@@ -20,8 +20,7 @@ const get_game_state = (game_id)=>
   db.one('SELECT game_status FROM games as g WHERE g.game_id=${game_id}',{game_id});
 
 const get_player_card_count = (game_id,user_id) => {
-  // get list of players in game_id
-  // list.getLength
+  db.one('SELECT user_id, count(hand_has_cards.card_id) AS number_of_cards_in_hand FROM hand_has_cards, game_has_hands WHERE hand_has_cards.hand_id = game_has_hands.hand_id AND game_id = ' + game_id + ' GROUP BY user_id').catch( error=> console.log("ERROR: ",error));
 };
 
 // checks if this user is able to make a play
@@ -37,6 +36,8 @@ const check_user_has_card = (game_id,user_id,card_id) =>{
 
 // sees if the card provided is playable agains the active card
 const check_playable = (game_id,card_id) =>{
+ db.one('SELECT * FROM active_card WHERE color = (SELECT color FROM cards WHERE card_id = ' + card_id + ') OR face = (SELECT face FROM cards WHERE card_id = ' + card_id + ') AND game_id = ' + game_id + '')
+  .catch( error=> console.log("ERROR: ",error));
   //    ! not proper implementation !
   // var active = db.one('SELECT top_card FROM game_id'); //<<<<<<<<<<<<<<<<<<<<< top_card or active_card
   // if( active.color == card_id.color || active.value == card_id.value || card_id > 99)
@@ -46,17 +47,18 @@ const check_playable = (game_id,card_id) =>{
 
 // sees if anyone has won in the game provided
 const check_win = (game_id)=>{
-  // check all players for hand with 0 cards
+  db.one('SELECT user_id FROM game_has_hands WHERE hand_id NOT IN (SELECT hand_id FROM hand_has_cards) AND game_id =' + game_id + '').catch( error=> console.log("ERROR: ",error));
 };
 
-const discard_card = (game_id,card_id)=>{
-  // add card_id to game_id.discard
+
+
+const discard_card = (game_id,user_id,card_id)=>{
+  db.one('DELETE FROM hand_has_cards WHERE hand_id = (SELECT hand_id FROM game_has_hands WHERE game_id = ' + game_id + ' AND user_id = ' + user_id + ') AND card_id =  ' + card_id + '').catch( error=> console.log("ERROR: ",error));
 };
 
-const draw_card = (game_id)=>{
-  // check for zero chard deck
-    // shuffle_discard if needed
-  // remove card from game_id.deck and return it
+const draw_card = (game_id,num_of_cards,hand_id)=>{
+  const card_id db.one('DELET FROM active_pile WHERE card_id = (SELECT card_id FROM active_pile WHERE game_id = ' +  game_id + ' LIMIT ' + num_of_cards + ') RETURNING card_id').catch( error=> console.log("ERROR: ",error));
+  db.one('INSERT INTO hand_has_cards (hand_id,card_id) VALUES (' + hand_id + ',' + card_id + ')')
 };
 
 const card_to_hand = (game_id,user_id,card_id)=>{
